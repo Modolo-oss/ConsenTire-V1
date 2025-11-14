32cc1754d9cf53cf305d6347a7d7e054c5eb71ce
Modolo-oss <akusiapasij252@gmail.com>
2025-11-14 11:17:59 +0000

    New Update

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2e41e0ff609c80b3"; }
+/******/ 		__webpack_require__.h = function() { return "e807f541016118c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
